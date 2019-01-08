7a93f39d4793c006ef032635ffc7c58987e8a394
Samuel Cornet <samuel@noukod.com>
2019-01-08 15:57:28 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,20 +1,4 @@
-# class PigLatinizer
-#
-#     def piglatinize words
-#       word_array = words.split ' '
-#       word_array.map do |word|
-#         if word.scan(/^[aeoui]+/).empty? == false
-#            result = word + "way"
-#            result
-#         else
-#           g = word.scan(/^[aeoui]+/).join ''
-#           word[0,g.size] = ''
-#            word + g + "ay"
-#           # result2
-#         end
-#       end.join" "
-#     end
-#   end
+
 class PigLatinizer
 
   def piglatinize word
@@ -23,9 +7,9 @@ class PigLatinizer
       if word_array.downcase.scan(/^[aeoui]+/).empty? == false
         word+"way"
       else
-        w = word.scan(/^[^aeoui]+/).join ''
-        word[0,w.size] = ''
-        word + w + "ay"
+        g = word.scan(/^[^aeoui]+/).join ''
+        word[0,g.size] = ''
+        word + g + "ay"
       end
     end.join ' '
 
