41e11af73bfe9d1a23c8061287666381e42c9e5b
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-11 15:57:34 +0000

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,20 @@
+class PigLatinizer
+
+  def piglatinize word
+
+    a = word.split(" ")
+        result = []
+
+        a.each do |words|
+          words_arr = words.scan(/\w/)
+          if words_arr.first.scan(/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]+/).size > 0
+            b = words.scan(/[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]+/).first
+            words_str = words.sub!(b, "")
+            result << words_str + b + "ay"
+          else
+            result << words + "way"
+          end
+        end
+    result.join(' ')
+  end
+end
