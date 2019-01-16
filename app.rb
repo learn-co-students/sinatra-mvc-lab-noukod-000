65d32bbefb6ed06fb109dc5c04a2431cbd1a0e5d
clifford <clifford@noukod.com>
2019-01-16 18:40:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+   	  get '/' do
+    erb :user_input
+  end
+
+   post '/piglatinize' do
+    @instance = PigLatinizer.new
+    @piglatinized = @instance.piglatinize(params[:user_phrase])
+  end
 end
